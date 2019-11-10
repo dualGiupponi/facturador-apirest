16de9e624d4ba958585b5a1bf5904110451f413a
Dual Miguel Giupponi <dual.giupponi@gmail.com>
2019-11-10 12:34:15 -0300

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(PORT);
   Logger.log(`App funcionando en el puerto ${PORT}`);
 }
